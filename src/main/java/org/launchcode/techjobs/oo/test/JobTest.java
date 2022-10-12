5e42346adb9f2d96699099170c4014fda59b8cbb
megochoa <102236786+megochoa@users.noreply.github.com>
2022-10-12 13:34:36 -0500

    Working through creating tests for the Job class.

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,43 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import javax.naming.Name;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId(){
+        Job firstJob = new Job();
+        Job secondJob = new Job();
+        assertNotEquals(firstJob.getId(), secondJob.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job = new Job();
+        Job jobCartoon = new Job(job.getId(),"Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertTrue(jobCartoon.getEmployer() instanceof Employer);
+        assertTrue(jobCartoon.getLocation() instanceof Location);
+        assertTrue(jobCartoon.getPositionType() instanceof PositionType);
+        assertTrue(jobCartoon.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(jobCartoon.getName() instanceof String);
+
+        assertEquals("Product tester", jobCartoon.getName());
+        assertEquals(("ACME"), jobCartoon.getEmployer().getValue());
+        assertEquals(("Desert"), jobCartoon.getLocation().getValue());
+        assertEquals(("Quality control"), jobCartoon.getPositionType().getValue());
+        assertEquals(("Persistence"), jobCartoon.getCoreCompetency().getValue());
+    }
 
 }
